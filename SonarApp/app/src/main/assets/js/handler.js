fd7ec2b0e010d61e4a33c51036c1011e6a321b64
AlexGolovko <golovkobalak@gmail.com>
2020-04-23 21:08:12 +0300

    added change color for failed connection

--- SonarApp/app/src/main/assets/js/handler.js
+++ SonarApp/app/src/main/assets/js/handler.js
@@ -1,26 +1,42 @@
 "use strict";
-let interval;
+var interval;
+var testPoint = 'http://localhost:8080/sonar';
+var prodPoint = 'http://192.168.4.1/';
 
-function updateDepth() {
-    var testPoint = 'http://localhost:8080/sonar';
-    var prodPoint = 'http://192.168.4.1/';
+function updateDepth(endpoind) {
     interval = setInterval(function () { callSonar() }, 1000);
     function callSonar() {
-        $.getJSON(prodPoint, function (data) {
+        $.getJSON(endpoind, function (data) {
+            $(".label").css("color", "white");
             var item;
-
             $.each(data, function (key, val) {
                 if (key == "depth") {
-                    item = "<p id='" + key + "' class='depth'>" + val + "</p>";
+                    item = "<p  class='depth'>" + parseFloat(val).toFixed(2) + "</p>";
                 }
             });
             $(".depth").first().before(item);
-            var n = $(".depth").length;
-            if (n > 10) {
+
+            if ($(".depth").length > 6) {
                 $(".depth").last().remove()
             }
+        }).catch(function () {
+            $(".label").css("color", "red");
         });
     }
+}
+
+function callEndpoint() {
+    $(".label").css("color", "red");
+    Promise.resolve(
+        $.ajax({
+            url: prodPoint,
+            timeout: 1000,//1 second timeout
+        })
+    ).then(function () {
+        updateDepth(prodPoint)
+    }).catch(function (e) {
+        updateDepth(testPoint)
+    });
 }
 
 function goToApp() {
--- SonarApp/app/src/main/java/com/example/sonarapp/MainActivity.java
+++ SonarApp/app/src/main/java/com/example/sonarapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sonarapp;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.webkit.WebView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
         View button = findViewById(R.id.button_sonar);
+
     }
 
     public void connect(View view){
