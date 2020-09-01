6671cc617c4bf13446ce1173801b7acf259a2d6f
AlexGolovko <golovkobalak@gmail.com>
2020-09-01 23:12:33 +0300

    Fixed build

--- SonarApp/app/src/androidTest/java/com/golovkobalak/sonarapp/ExampleInstrumentedTest.java
+++ SonarApp/app/src/androidTest/java/com/golovkobalak/sonarapp/ExampleInstrumentedTest.java
@@ -21,7 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        assertEquals("com.example.sonarapp", appContext.getPackageName());
+        assertEquals("com.golovkobalak.sonarapp", appContext.getPackageName());
     }
 }
--- SonarApp/app/src/androidTest/java/com/golovkobalak/sonarapp/MainActivityTest.java
+++ SonarApp/app/src/androidTest/java/com/golovkobalak/sonarapp/MainActivityTest.java
@@ -1,4 +1,48 @@
 package com.golovkobalak.sonarapp;
 
+import android.graphics.Point;
+import android.os.RemoteException;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityTestRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void init(){
+        UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        try {
+            if (!uiDevice.isScreenOn()) {
+                uiDevice.wakeUp();
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void checkContainerIsDisplayed() {
+        ActivityScenario<MainActivity> scenario = activityTestRule.getScenario();
+        onView(ViewMatchers.withId(R.id.container))
+                .check(matches(isDisplayed()));
+
+    }
 }
