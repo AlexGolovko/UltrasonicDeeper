d79f3ce5ebd8d9b363db49e3b368cfe4ff8ae5d7
AlexGolovko <golovkobalak@gmail.com>
2022-09-14 21:51:20 +0300

    #11 - Migrate to REST

--- SonarApp/app/src/main/java/com/golovkobalak/sonarapp/controller/TrackingController.java
+++ SonarApp/app/src/main/java/com/golovkobalak/sonarapp/controller/TrackingController.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.util.Log;
 
 import com.golovkobalak.sonarapp.model.GeoSquare;
+import com.golovkobalak.sonarapp.model.Marker;
 import com.golovkobalak.sonarapp.service.TrackingService;
+import com.google.gson.Gson;
 
 import java.util.List;
 import java.util.Map;
@@ -14,30 +16,36 @@ import io.javalin.Javalin;
 public class TrackingController {
 
     private static final String TAG = TrackingController.class.getName();
+    private static final Gson gson = new Gson();
+    public static final int PORT = 8080;
     private final Javalin app;
     private final TrackingService trackingService;
 
     public TrackingController(Context context) {
-        this.app = Javalin.create().start(8080);
+        this.app = Javalin.create().start(PORT);
         this.trackingService = new TrackingService(context);
         app.before(ctx -> {
             Log.d(TAG, ctx.fullUrl());
             Log.d(TAG, ctx.body());
         });
+        //POST http://localhost:8080/tracking/save
         app.post("/tracking/save", ctx -> {
             final String body = ctx.body();
             trackingService.saveTrackingList(body);
             ctx.status(201);
         });
+        //GET http://localhost:8080/system/map/cache/dir
         app.get("/system/map/cache/dir", ctx -> {
             final String mapCacheDir = trackingService.getMapCacheDir();
             ctx.result(mapCacheDir);
         });
+        //GET http://localhost:8080/marker?north=49.960455723200724&east=36.34042262789566&south=49.955769014252176&west=36.33620619532426
         app.get("/marker", ctx -> {
             final Map<String, List<String>> queryParamMap = ctx.queryParamMap();
             final GeoSquare geoSquare = new GeoSquare(queryParamMap);
-            final String markers = trackingService.getMarkers(geoSquare);
-            ctx.result(markers);
+            final List<Marker> markers = trackingService.getMarkers(geoSquare);
+
+            ctx.result(gson.toJson(markers));
         });
     }
 
--- SonarApp/app/src/main/java/com/golovkobalak/sonarapp/service/TrackingService.java
+++ SonarApp/app/src/main/java/com/golovkobalak/sonarapp/service/TrackingService.java
@@ -2,7 +2,6 @@ package com.golovkobalak.sonarapp.service;
 
 import android.content.Context;
 import android.util.Log;
-import android.webkit.JavascriptInterface;
 
 import com.golovkobalak.sonarapp.model.GeoSquare;
 import com.golovkobalak.sonarapp.model.Marker;
@@ -13,24 +12,17 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import okhttp3.OkHttpClient;
 
 public class TrackingService {
     private static final String TAG = TrackingService.class.getSimpleName();
     private final Context context;
     private static final Gson gson = new Gson();
     private static final SonarDataRepository repo = new SonarDataRepository();
-    private static final AtomicBoolean isCanceled = new AtomicBoolean(false);
-    private static final ConcurrentHashMap<Thread, OkHttpClient> THREAD_OK_HTTP_CLIENT_CONCURRENT_HASH_MAP = new ConcurrentHashMap<>();
 
     public TrackingService(Context context) {
         this.context = context;
     }
 
-    @JavascriptInterface
     public void saveTrackingList(String data) {
         Log.i(TAG, "saveTrackingList: " + data);
         try {
@@ -41,21 +33,12 @@ public class TrackingService {
         }
     }
 
-    @JavascriptInterface
     public String getMapCacheDir() {
         String filesPath = context.getFilesDir().getAbsolutePath();
         return "file://" + filesPath + "/Tiles";
     }
 
-    @JavascriptInterface
-    public String findMarkers(String data) {
-        Log.d(TAG, "findMarkers: " + data);
-        //"{"north":49.960455723200724,"east":36.34042262789566,"south":49.955769014252176,"west":36.33620619532426}"
-        final GeoSquare geoSquare = gson.fromJson(data, GeoSquare.class);
-        return getMarkers(geoSquare);
-    }
-
-    public String getMarkers(GeoSquare geoSquare) {
+    public List<Marker> getMarkers(GeoSquare geoSquare) {
         final ArrayList<Marker> list = new ArrayList<>();
         try {
             final List<SonarData> markers = repo.findByGeoSquare(geoSquare);
@@ -65,8 +48,6 @@ public class TrackingService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        final String response = gson.toJson(list);
-        Log.d(TAG, "response:" + response);
-        return response;
+        return list;
     }
 }
