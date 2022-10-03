60e06225e4a2e288101c187870869aa9735c11c2
AlexGolovko <golovkobalak@gmail.com>
2022-10-03 22:38:32 +0300

    #27 - Move Javalin controllers and SonarContext to kt

--- SonarApp/app/src/main/java/com/golovkobalak/sonarapp/SonarContext.kt
+++ SonarApp/app/src/main/java/com/golovkobalak/sonarapp/SonarContext.kt
@@ -1,27 +1,11 @@
-package com.golovkobalak.sonarapp;
+package com.golovkobalak.sonarapp
 
-import android.content.res.AssetManager;
+import android.content.res.AssetManager
 
-public class SonarContext {
-    public static AssetManager getAssetManager() {
-        return assetManager;
-    }
+object SonarContext {
+    @JvmStatic
+    var assetManager: AssetManager? = null
 
-    public static void setAssetManager(AssetManager assetManager) {
-        SonarContext.assetManager = assetManager;
-    }
-
-    public static String getFilesDirAbsPath() {
-        return filesDirAbsPath;
-    }
-
-    public static void setFilesDirAbsPath(String filesDirAbsPath) {
-        SonarContext.filesDirAbsPath = filesDirAbsPath;
-    }
-
-    private static AssetManager assetManager;
-    private static String filesDirAbsPath;
-
-    private SonarContext() {
-    }
+    @JvmStatic
+    var filesDirAbsPath: String? = null
 }
--- SonarApp/app/src/main/java/com/golovkobalak/sonarapp/controller/TrackingController.kt
+++ SonarApp/app/src/main/java/com/golovkobalak/sonarapp/controller/TrackingController.kt
@@ -1,59 +1,56 @@
-package com.golovkobalak.sonarapp.controller;
+package com.golovkobalak.sonarapp.controller
 
-import android.util.Log;
+import android.util.Log
+import com.golovkobalak.sonarapp.model.GeoSquare
+import com.golovkobalak.sonarapp.service.TrackingService
+import com.google.gson.Gson
+import io.javalin.Javalin
+import io.javalin.core.JavalinConfig
+import io.javalin.http.Context
+import io.javalin.http.Handler
 
-import com.golovkobalak.sonarapp.model.GeoSquare;
-import com.golovkobalak.sonarapp.model.Marker;
-import com.golovkobalak.sonarapp.service.TrackingService;
-import com.google.gson.Gson;
-
-import java.util.List;
-import java.util.Map;
-
-import io.javalin.Javalin;
-import io.javalin.core.JavalinConfig;
-
-public class TrackingController {
-
-    private static final String TAG = TrackingController.class.getName();
-    private static final Gson gson = new Gson();
-    public static final int PORT = 8080;
-    private Javalin app;
-
-    public void start() {
-        this.app = Javalin.create(JavalinConfig::enableCorsForAllOrigins).start(PORT);
-        TrackingService trackingService = new TrackingService();
-        app.before(ctx -> {
-            Log.d(TAG, ctx.fullUrl());
-            Log.d(TAG, ctx.body());
-        });
+class TrackingController {
+    private var app: Javalin? = null
+    fun start() {
+        val javalin = Javalin.create { obj: JavalinConfig -> obj.enableCorsForAllOrigins() }.start(PORT)
+        val trackingService = TrackingService()
+        javalin.before(Handler { ctx: Context ->
+            Log.d(TAG, ctx.fullUrl())
+            Log.d(TAG, ctx.body())
+        })
         //POST http://localhost:8080/tracking/save
-        app.post("/tracking", ctx -> {
-            final String body = ctx.body();
-            trackingService.saveTrackingList(body);
-            ctx.status(201);
-        });
+        javalin.post("/tracking", Handler { ctx: Context ->
+            val body = ctx.body()
+            trackingService.saveTrackingList(body)
+            ctx.status(201)
+        })
         //GET http://localhost:8080/system/map/cache/dir
-        app.get("/system/mapCacheDir", ctx -> {
-            final String mapCacheDir = trackingService.getMapCacheDir();
-            ctx.result(gson.toJson(mapCacheDir));
-        });
+        javalin.get("/system/mapCacheDir", Handler { ctx: Context ->
+            val mapCacheDir = trackingService.mapCacheDir
+            ctx.result(gson.toJson(mapCacheDir))
+        })
         //GET http://localhost:8080/marker?north=49.960455723200724&east=36.34042262789566&south=49.955769014252176&west=36.33620619532426
-        app.get("/marker", ctx -> {
-            final Map<String, List<String>> queryParamMap = ctx.queryParamMap();
-            final GeoSquare geoSquare = new GeoSquare(queryParamMap);
-            final List<Marker> markers = trackingService.getMarkers(geoSquare);
-
-            ctx.result(gson.toJson(markers));
-        });
-        app.after(ctx -> {
-            Log.d(TAG, ctx.fullUrl());
-            Log.d(TAG, ctx.body());
-        });
-        Log.i(this.getClass().getName(), "end creation");
+        javalin.get("/marker", Handler { ctx: Context ->
+            val queryParamMap = ctx.queryParamMap()
+            val geoSquare = GeoSquare(queryParamMap)
+            val markers = trackingService.getMarkers(geoSquare)
+            ctx.result(gson.toJson(markers))
+        })
+        javalin.after(Handler { ctx: Context ->
+            Log.d(TAG, ctx.fullUrl())
+            Log.d(TAG, ctx.body())
+        })
+        app = javalin
+        Log.i(this.javaClass.name, "end creation")
     }
 
-    public void destroy() {
-        app.close();
+    fun destroy() {
+        app?.close()
+    }
+
+    companion object {
+        private val TAG = TrackingController::class.java.name
+        private val gson = Gson()
+        const val PORT = 8080
     }
 }
