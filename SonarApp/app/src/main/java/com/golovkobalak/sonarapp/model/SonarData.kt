6a5a636221e126e5d34c1a8ef1051eb0fbbe76e5
AlexGolovko <golovkobalak@gmail.com>
2022-10-08 23:05:52 +0300

    #27 - migration models

--- SonarApp/app/src/main/java/com/golovkobalak/sonarapp/model/SonarData.kt
+++ SonarApp/app/src/main/java/com/golovkobalak/sonarapp/model/SonarData.kt
@@ -1,121 +1,21 @@
-package com.golovkobalak.sonarapp.model;
+package com.golovkobalak.sonarapp.model
 
-import com.golovkobalak.sonarapp.MainActivity;
+import com.golovkobalak.sonarapp.MainActivity
+import io.realm.RealmObject
 
-import io.realm.RealmObject;
+open class SonarData(
+    var depth: String, var battery: String, var temperature: String,
+    var time: String, var altitude: String, var accuracy: String
+) : RealmObject() {
+    var sessionId = MainActivity.SESSION_ID
+    var altitudeAccuracy: String? = null
+    var heading: String? = null
+    var latitude = 0.0
+    var longitude = 0.0
+    var speed: String? = null
 
-public class SonarData extends RealmObject {
-    private String sessionId = MainActivity.SESSION_ID;
-    private String depth;
-    private String battery;
-    private String temperature;
-    private String time;
-    private String accuracy;
-    private String altitude;
-    private String altitudeAccuracy;
-    private String heading;
-    private double latitude;
-    private double longitude;
-    private String speed;
-
-    public String getDepth() {
-        return depth;
-    }
-
-    public void setDepth(String depth) {
-        this.depth = depth;
-    }
-
-    public String getBattery() {
-        return battery;
-    }
-
-    public void setBattery(String battery) {
-        this.battery = battery;
-    }
-
-    public String getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(String temperature) {
-        this.temperature = temperature;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getSessionId() {
-        return sessionId;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-
-    public String getAccuracy() {
-        return accuracy;
-    }
-
-    public void setAccuracy(String accuracy) {
-        this.accuracy = accuracy;
-    }
-
-    public String getAltitude() {
-        return altitude;
-    }
-
-    public void setAltitude(String altitude) {
-        this.altitude = altitude;
-    }
-
-    public String getAltitudeAccuracy() {
-        return altitudeAccuracy;
-    }
-
-    public void setAltitudeAccuracy(String altitudeAccuracy) {
-        this.altitudeAccuracy = altitudeAccuracy;
-    }
-
-    public String getHeading() {
-        return heading;
-    }
-
-    public void setHeading(String heading) {
-        this.heading = heading;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(String speed) {
-        this.speed = speed;
-    }
-
-    public static class Field {
-        public static final String LATITUDE = "latitude";
-        public static final String LONGITUDE = "longitude";
+    object Field {
+        const val LATITUDE = "latitude"
+        const val LONGITUDE = "longitude"
     }
 }
