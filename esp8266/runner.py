dcbf3aa59ab25bc912eb39a894c92cfd7a4d1bf5
AlexGolovko <golovkobalak@gmail.com>
2022-12-29 20:25:56 +0200

    update runner

--- esp8266/runner.py
+++ esp8266/runner.py
@@ -1,19 +1,25 @@
-import uasyncio
-
+import uasyncio as asyncio
 from microDNSSrv import dns
 from userver import httpServer
-from ws_sonar_server import WSServer
 import sensorservice as sensorservice
 import logger, blink as blinker
+from wsserver import WSServer
+from wsservice import WSService
 
 
 def run():
     try:
-        dns()
-        httpServer()
-        service = sensorservice.run()
-        WSServer(callback=service.callback).run()
-        uasyncio.run(blinker.blink())
+        # dns()
+        # httpServer()
+        # service = sensorservice.run()
+        # WSServer(callback=service.callback).run()
+        # uasyncio.run(blinker.blink())
+        service = WSService()
+        server = WSServer(service)
+        asyncio.run(server.run())
     except Exception as err:
         logger.error(err)
         raise err
+    finally:
+        asyncio.run(server.close())
+        _ = asyncio.new_event_loop()
