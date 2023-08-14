6cefe0b2e9b16f0d7c7615127add7e7b3bb06ef0
AlexGolovko <golovkobalak@gmail.com>
2023-08-14 19:09:45 +0300

    33 - Migrate TilesController

---
+++ Android/app/src/main/java/com/gmail/golovkobalak/sonar/controller/TilesController.kt
@@ -0,0 +1,43 @@
+package com.gmail.golovkobalak.sonar.controller
+
+import android.util.Log
+import com.gmail.golovkobalak.sonar.config.GlobalContext
+import io.javalin.Javalin
+import io.javalin.core.JavalinConfig
+import java.io.File
+
+class TilesController {
+    private val TAG = TilesController::class.java.name
+
+    private var app: Javalin? = null
+    fun start() {
+        app = Javalin.create{ obj: JavalinConfig -> obj.enableCorsForAllOrigins() }.start(7000)
+        // Add the "/{z}/{x}/{y}.png" route and associate it with the handler
+        app!!.get("/{z}/{x}/{y}.png") { ctx ->
+            // Extract the z, x, and y values from the path parameters
+            val z = ctx.pathParam("z")
+            val x = ctx.pathParam("x")
+            val y = ctx.pathParam("y")
+
+            // Generate the path to the image file
+            val imagePath = GlobalContext.filesDirAbsPath + "/Tiles/$z/$x/$y.png"
+            Log.d(TAG, "imagePath: $imagePath")
+            // Check if the file exists
+            val imageFile = File(imagePath)
+            if (imageFile.exists() && imageFile.isFile) {
+                // Set the Content-Type header to "image/png"
+                ctx.contentType("image/png")
+                // Return the image file
+                ctx.result(imageFile.readBytes())
+            } else {
+                // If the file doesn't exist, return a 404 Not Found response
+                ctx.status(404).result("Tile not found")
+            }
+        }
+        Log.d(TAG, "started")
+    }
+    fun destroy() {
+        app!!.stop()
+    }
+
+}
--- Android/app/src/main/java/com/gmail/golovkobalak/sonar/service/Runner.kt
+++ Android/app/src/main/java/com/gmail/golovkobalak/sonar/service/Runner.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.content.res.AssetManager
 import com.gmail.golovkobalak.sonar.config.GlobalContext
 import com.gmail.golovkobalak.sonar.controller.SonarController
+import com.gmail.golovkobalak.sonar.controller.TilesController
 
 object Runner {
     val sonarController: SonarController = SonarController()
+    val tilesController: TilesController = TilesController()
     fun start(assets: AssetManager, baseContext: Context) {
         GlobalContext.assetManager = assets
         GlobalContext.filesDirAbsPath = baseContext.filesDir.absolutePath
         sonarController.start()
+        tilesController.start()
     }
 
 }
