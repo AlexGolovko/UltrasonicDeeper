d6f162a5be3c127786e377c987dc32998ae1575e
oleksandr.holovko <golovkobalak@gmail.com>
2023-10-07 21:29:30 +0300

    33 - Add test project to simulate sonar

---
+++ SonarMockServer/src/test/kotlin/com/golovkobalak/sonarmockserver/SonarMockServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.golovkobalak.sonarmockserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SonarMockServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
