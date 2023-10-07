0370ef27f552df27d12adf564743a271cd5dca80
oleksandr.holovko <golovkobalak@gmail.com>
2023-10-08 00:02:35 +0300

    33 - Update version

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1"
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
