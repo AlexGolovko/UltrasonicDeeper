885b132d4fe7a9fd29b9a4c12ba80ac922df8d30
oleksandr.holovko <golovkobalak@gmail.com>
2023-10-11 19:31:35 +0300

    33 - Use BuildConfig for env to define diff build configs

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "0.6.0"
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
